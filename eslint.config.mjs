4ba0bad9f52bf42e8f905829e83e5ae38762ae5a
quantam101 <222719326+quantam101@users.noreply.github.com>
2026-05-13 05:36:28 -0700

    Fix ESLint config path import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import path from "node:util";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
